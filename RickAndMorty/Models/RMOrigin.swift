f30921a5a0451fa9f2e59ce401eedc9a938f22fc
Ismael Aquino <ismaellAquino@hotmail.com>
2023-10-01 00:15:08 -0300

    part 03

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by IsMalware on 30/09/23.
+//
+
+import Foundation
+
+struct RMOrign : Codable {
+    let name: String
+    let url: String
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by IsMalware on 30/09/23.
+//
+
+import Foundation
+
+struct RMSingleLocation : Codable {
+    let name: String
+    let url: String
+}
